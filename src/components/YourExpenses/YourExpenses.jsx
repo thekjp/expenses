7b5e269f4b5c2c9f0afc29ee1d6da581462a9bce
thekjp <kristianjp@icloud.com>
2024-06-08 15:39:15 -0700

    error message if array is empty / set unique id for key

--- src/components/YourExpenses/YourExpenses.jsx
+++ src/components/YourExpenses/YourExpenses.jsx
@@ -17,34 +17,37 @@ function YourExpenses() {
         console.error("Error fetching users expenses");
       }
     };
-
     getExpenses();
   }, []);
 
   return (
     <div className="your-expenses">
       <h2 className="your-expenses__title">Your Expenses</h2>
-      <ul className="your-expenses__list">
-        {expenses?.map((expense) => (
-          <li className="your-expenses__list-item" key={expense.id}>
-            <div className="your-expenses__item">
-              <h3 className="your-expenses__subtitle">{expense.title}</h3>
-              <p className="your-expenses__text your-expenses__text--amount">
-                Amount: ${expense.amount}
-              </p>
-              <p className="your-expenses__text your-expenses__text--total">
-                Total: ${expense.total_amount}
-              </p>
-              <p className="your-expenses__text your-expenses__text--date">
-                Date: ${new Date(expense.date).toLocaleDateString()}
-              </p>
-              <p className="your-expenses__text your-expenses__text--group">
-                Group: ${expense.group_name}
-              </p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {expenses.length === 0 ? (
+        <p className="your-expenses__error">No expenses found.</p>
+      ) : (
+        <ul className="your-expenses__list">
+          {expenses?.map((expense) => (
+            <li className="your-expenses__list-item" key={expense.expense_id}>
+              <div className="your-expenses__item">
+                <h3 className="your-expenses__subtitle">{expense.title}</h3>
+                <p className="your-expenses__text your-expenses__text--amount">
+                  Amount: ${expense.amount}
+                </p>
+                <p className="your-expenses__text your-expenses__text--total">
+                  Total: ${expense.total_amount}
+                </p>
+                <p className="your-expenses__text your-expenses__text--date">
+                  Date: ${new Date(expense.date).toLocaleDateString()}
+                </p>
+                <p className="your-expenses__text your-expenses__text--group">
+                  Group: ${expense.group_name}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
